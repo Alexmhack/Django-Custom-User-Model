3620eeea182b3ba55a3fdfdcbc17439e038d18be
alexHooper <alexmohack@gmail.com>
2018-10-28 18:56:06 +0530

    add user manager for custom model

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,48 @@ from django.contrib.auth.models import (
 	BaseUserManager, AbstractBaseUser
 )
 
+class UserManager(BaseUserManager):
+	def create_user(self, email, password=None):
+		"""
+		creates a user with given email and password
+		"""
+		if not email:
+			raise ValueError('user must have a email address')
+
+		user = self.model(
+			email=self.normalize_email(email),
+		)
+
+		user.set_password(password)
+		user.save(self._db)
+		return user
+
+	def create_staffuser(self, email, password):
+		"""
+		creates a user with staff permissions
+		"""
+		user = self.create_user(
+			email=email,
+			password=password
+		)
+		user.staff = True
+		user.save(using=self._db)
+		return user
+
+	def create_superuser(self, email, password):
+		"""
+		creates a superuser with email and password
+		"""
+		user = self.create_user(
+			email=email,
+			password=password
+		)
+		user.staff = True
+		user.admin = True
+		user.save(using=self._db)
+		return user
+
+
 class User(AbstractBaseUser):
 	email = models.EmailField(
 		verbose_name='Email address',
@@ -45,3 +87,6 @@ class User(AbstractBaseUser):
 	def is_active(self):
 		"""Is the user active"""
 		return self.active
+
+	# hook the user manager to objects
+	objects = UserManager()
